d4c6691b463edb9f72b953e2616bbc4f0959873e
Felipe Freitas <soufreitas.felipe@gmail.com>
2023-08-09 19:53:12 -0300

    criação de um template unico utilizando layout thymeleaf

--- src/main/java/br/com/alura/screenmatch/controller/FilmeController.java
+++ src/main/java/br/com/alura/screenmatch/controller/FilmeController.java
@@ -1,15 +1,44 @@
 package br.com.alura.screenmatch.controller;
 
+import br.com.alura.screenmatch.model.filme.DadosCadastroFilme;
+import br.com.alura.screenmatch.model.filme.Filme;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/filmes")
 public class FilmeController {
 
-      @GetMapping
-      public String carregaPaginaFormulario (){
+    private List<Filme> filmes = new ArrayList<>();
+
+    @GetMapping
+    public String carregaPaginaListagem (Model model){
+
+        model.addAttribute("lista", filmes);
+
+        return "filmes/listagem";
+    }
+
+    @GetMapping("/formulario")
+    public String carregaPaginaFormulario (){
               return "filmes/formulario";
       }
+
+      @PostMapping
+      public String cadastraFilme (DadosCadastroFilme dados){
+//          System.out.println(dados);
+          //crio um objeto filme para preencher
+          var filme = new Filme(dados);
+          filmes.add(filme);
+
+          return "redirect:/filmes";
+      }
+
+
 }
---
+++ src/main/java/br/com/alura/screenmatch/model/filme/DadosCadastroFilme.java
@@ -0,0 +1,3 @@
+package br.com.alura.screenmatch.model.filme;
+
+public record DadosCadastroFilme(String nome,Integer duracao, Integer ano, String genero ) {}
